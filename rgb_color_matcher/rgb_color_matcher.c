fe04288157dea2e5d69a3a60968096d26d28ff31
PaulBoardman <PaulBoardman@5ec23c0e-d09d-11de-8f89-a3abdf1de2f0>
2010-01-27 07:48:27 +0000

    better way to deal with the pots.

--- rgb_color_matcher/rgb_color_matcher.c
+++ rgb_color_matcher/rgb_color_matcher.c
@@ -5,36 +5,30 @@
 #include <avr/eeprom.h>
 #include <math.h> //for square root function
 
-#define LED_RED_AUTO PD7 // pin 17 (atmega)
+#define LED_RED_AUTO PD7
 #define LED_GREEN_AUTO PD6
 #define LED_BLUE_AUTO PD5
 #define LED_RED_USER PD4
 #define LED_GREEN_USER PD3
 #define LED_BLUE_USER PD2
-#define OUTPUT_MASK_AUTO (1 << PD7) | (1 << PD6) | (1 << PD5)
-#define OUTPUT_MASK_USER (1 << PD4) | (1 << PD3) | (1 << PD2)
+#define OUTPUT_MASK_AUTO (1 << LED_RED_AUTO) | (1 << LED_GREEN_AUTO) | (1 << LED_BLUE_AUTO)
+#define OUTPUT_MASK_USER (1 << LED_RED_USER) | (1 << LED_GREEN_USER) | (1 << LED_BLUE_USER)
 #define SPEAKER_PIN PB3
-#define RED_POT PC0 //adc0
+#define RED_POT PC0
 #define GREEN_POT PC1
 #define BLUE_POT PC2
-#define INPUT_MASK ((1 << PC0) | (1 << PC1) | (1 << PC2))
-#define PWM_MIN 0
-#define PWM_MAX 225
+#define INPUT_MASK ((1 << RED_POT) | (1 << GREEN_POT) | (1 << BLUE_POT))
 
 //define the processor speed (if it's not already been defined by the compiler)
 #ifndef F_CPU
 	#define F_CPU 8000000UL
 #endif
-/**
- * TODO add switch for adjusting the difficulty (speaker on/off).
- */
-uint8_t mUserLedLevels[3] = {0,0,0};
-uint8_t mAutoLedLevels[3] = {150, 0, 30};
+
+uint8_t mUserLedLevels[3] = {0, 0, 0};
+uint8_t mAutoLedLevels[3] = {0, 0, 0};
 uint8_t mBuffer[6];
 uint8_t i;
 uint8_t kDelta = 50;
-uint8_t mAdcChannel[3] = {RED_POT, GREEN_POT, BLUE_POT}; //is this correct?
-uint8_t mSpeakerOn = 1;
 
 uint8_t readAdc(uint8_t pChannel)
 {
@@ -49,7 +43,7 @@ uint8_t readAdc(uint8_t pChannel)
 
 	while(!(ADCSRA & (1 << ADIF)));
 
-	return(ADCH);
+	return(255 - ADCH);
 }
 
 void initTimers()
@@ -66,7 +60,7 @@ void initTimers()
 	//start timer0, no prescale
 	TCCR0 = (1 << CS00);
 
-	//set ctc mode for timer2 PWM
+	//set CTC mode for timer2 PWM
 	TCCR2 |= (1 << WGM21);
 
 	//set toggle logical level on each compare match
@@ -88,8 +82,6 @@ void initAdc()
 	// Select divider factor 8, so we get 1 MHz/8 = 125 kHz ADC clock
 	ADCSRA |= (1<<ADPS1) | (1<<ADPS0);
 
-	// we'll select the channel each time we read the ADC
-
 	// Use Vcc as voltage reference
 	ADMUX |= (1 << REFS0);
 
@@ -106,7 +98,10 @@ void setAutoLeds()
 	}
 }
 
-//use a variable in EEPROM
+/*
+ * Use a variable stored in EEPROM to ensure the random color
+ * sequence changes from one game to the next.
+ */
 void initRand()
 {
 	uint8_t vSeed = eeprom_read_word(0); // load last stored seed
@@ -114,27 +109,39 @@ void initRand()
 	eeprom_write_word(0, vSeed); //store the new seed for next time
 }
 
+/*
+ * Flash the LEDs on a successful match.  We also
+ * modulate the speaker frequency.
+ */
 void flashAndReset()
 {
 	cli();
 	for(i=0;i<5;i++)
 	{
-		if(mSpeakerOn)
-		{
-			OCR2 = i * 50;
-		}
-		PORTD |= OUTPUT_MASK_AUTO;
+		//modulate speaker frequency on success
+		OCR2 = i * 50;
+
+		//flash the LEDs
+		PORTD |= (OUTPUT_MASK_AUTO | OUTPUT_MASK_USER);
+
 		_delay_ms(100);
-		PORTD &=~ OUTPUT_MASK_AUTO;
+		PORTD &=~ (OUTPUT_MASK_AUTO | OUTPUT_MASK_USER);
 		_delay_ms(100);
 	}
 	setAutoLeds();
 	sei();
 }
 
-//compare the RGB vectors for the two RGB LEDs
-//if they're within kDelta then flash and reset
-//to a new value
+/*
+ * Compare the RGB vectors of the two RGB LEDs.
+ * If they're within kDelta then flash and reset
+ * to a new value.
+ *
+ * We modulate the speaker frequency here as well
+ * to give an audio queue for the 'player'.  The
+ * speaker can be wired up to a difficulty switch
+ * to turn it off (outside of software).
+ */
 void compareValues()
 {
 	uint16_t vDistance = 0;
@@ -144,10 +151,8 @@ void compareValues()
 	}
 	vDistance = sqrt(vDistance);
 
-	if(mSpeakerOn)
-	{
-		OCR2 = vDistance/2;
-	}
+	//set the speaker frequency
+	OCR2 = vDistance/2;
 
 	if(vDistance <= kDelta)
 	{
@@ -157,7 +162,7 @@ void compareValues()
 
 int main(void)
 {
-	DDRD |= OUTPUT_MASK_AUTO | OUTPUT_MASK_USER;//led set as output
+	DDRD |= (OUTPUT_MASK_AUTO | OUTPUT_MASK_USER);//led set as output
 	DDRC &=~ INPUT_MASK;//set potentiometer pin for input
 	DDRB |= (1 << SPEAKER_PIN); //speaker output
 	initTimers();
@@ -167,11 +172,9 @@ int main(void)
 
 	while (1)
 	{
-		for(i = 0; i < 3; i++)
-		{
-			mUserLedLevels[i] = readAdc(i);
-			_delay_ms(5);
-		}
+		mUserLedLevels[0] = readAdc(BLUE_POT);
+		mUserLedLevels[1] = readAdc(GREEN_POT);
+		mUserLedLevels[2] = readAdc(RED_POT);
 		_delay_ms(10);
 		compareValues();
 	}
